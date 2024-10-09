7932ddefbdbe4cb082025e2f931e76a906999a0e
Your Name <you@example.com>
2024-10-09 12:54:51 +0300

    form data sent to confirmation.php and formatted

---
+++ web/confirmation.php
@@ -0,0 +1,11 @@
+<?php
+
+// echo "to be implemented";
+
+// var_dump($_POST);
+
+// echo $_POST["name"];
+
+echo $_POST["name"]
+    . "ntra, Jūs esat veiksmīgi reģistrējusies webināram ar e-pastu"
+    . $_POST["email"];
